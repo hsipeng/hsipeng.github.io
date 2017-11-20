29056aee10a3013892dfc1417cdaedf1d720a46b
lirawx <lira_wx@outlook.com>
2017-11-20 20:08:33 +0800

    genmd change to axios

--- genmd.js
+++ genmd.js
@@ -1,18 +1,6 @@
-var request = require('request');
 var fs = require('fs');
 var path = require('path');
-
-var options = {
-    url: 'https://api.github.com/repos/lirawx/mirror/issues',
-    headers: {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.79 Safari/537.36',
-        'Host':'api.github.com',
-        'DNT':'1',
-        'If-None-Match':'W/"074817d16634c58050520f5c85690abf"',
-        'Upgrade-Insecure-Requests':'1',
-        'Cache-Control':'0'
-    }
-};
+const api = require('./util/api')
 
 var getTags = function (arr){
     var tags ='';
@@ -43,7 +31,6 @@ var fsExistsSync =function (path) {
     return true;
 }
 var mkdirsSync = function (dirname){
-    console.log(dirname);
     if(fs.existsSync(dirname)){
         return true;
     }else{
@@ -54,42 +41,42 @@ var mkdirsSync = function (dirname){
     }
 }
 
-request(options, function (error, response, body) {
-    if (!error && response.statusCode == 200) {
-        var data = JSON.parse(body);
-        for (iss in data){
-            if(data[iss].state=='open'){
+api.get('lirawx/mirror/issues',{}).then(res =>{
+    let data = res.data
+    for (iss in data){
+        if(data[iss].state=='open'){
 
-                var article = '';
-                article+='---\n';
-                article+='layout: post\n';
-                article+='tags: '+getTags(data[iss].labels)+'\n';
-                article+='title: '+data[iss].title+'\n';
-                article+='date: '+data[iss].created_at+'\n';
-                article+='---\n\n';
-                article+=data[iss].body;
-                var fN = getFileName(data[iss].created_at,data[iss].title);
-                var filePath =calFilePath(data[iss].created_at,fN);
-                if(!fsExistsSync(filePath)){
+            var article = '';
+            article+='---\n';
+            article+='layout: post\n';
+            article+='tags: '+getTags(data[iss].labels)+'\n';
+            article+='title: '+data[iss].title+'\n';
+            article+='date: '+data[iss].created_at+'\n';
+            article+='---\n\n';
+            article+=data[iss].body;
+            var fN = getFileName(data[iss].created_at,data[iss].title);
+            var filePath =calFilePath(data[iss].created_at,fN);
+            if(!fsExistsSync(filePath)){
 
-                   mkdirsSync(filePath);
-                    var out = fs.createWriteStream(filePath+fN,{encoding:'utf-8','flag': 'a'});
-                    out.write(article);
-                    out.end();
-                    console.log(fN+'----- 已经写入');
-                }else if(!fsExistsSync(filePath+fN)){
-                    var outs = fs.createWriteStream(filePath+fN,{encoding:'utf-8','flag': 'a'});
-                    outs.write(article);
-                    outs.end();
-                    console.log(fN+'----- 已经写入');
-                }else{
-                    console.log(fN+'----- 已经存在');
-                }
+               if (mkdirsSync(filePath)){
+                var out = fs.createWriteStream(filePath+fN,{encoding:'utf-8','flag': 'a'});
+                out.write(article);
+                out.end();
+                console.log(fN+'----- 已经写入');
+               }
+                
+            }else if(!fsExistsSync(filePath+fN)){
+                var outs = fs.createWriteStream(filePath+fN,{encoding:'utf-8','flag': 'a'});
+                outs.write(article);
+                outs.end();
+                console.log(fN+'----- 已经写入');
+            }else{
+                console.log(fN+'----- 已经存在');
+            }
 
 
-        }
-        }
     }
-});
+    }
+  })
 
 
